2a94718ec3bf1d9be3359d8284080b19cfd0ea29
mm02417 <mm02417@surrey.ac.uk>
2021-07-22 15:29:07 +0100

    Adds scraping FAQ page and basic web server

---
+++ index.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+async function test() {
+    const response = await fetch('https://www.oracle.com/startup/faq/');
+    const text = await response.text();
+    const $ = cheerio.load(text);
+
+    let dict = {};
+
+    $('li > h5').each(function (i) {
+        dict[$(this).text()] = $('li > div').eq(i + 10).html();
+    });
+
+    console.log(dict['I have questions about my invoice. Who can I contact?']);
+}
+test();
